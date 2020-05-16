a0751ff0e33bbd24c472b3498600e8d97b416ca6
Ramy Rizkalla <rwagdyrizkalla@gmail.com>
2020-05-16 20:23:52 +0200

    Initial Commit

---
+++ PinView/PinView.h
@@ -0,0 +1,19 @@
+//
+//  PinView.h
+//  PinView
+//
+//  Created by Ramy Wagdy on 5/16/20.
+//  Copyright © 2020 Ramy Rizkalla. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PinView.
+FOUNDATION_EXPORT double PinViewVersionNumber;
+
+//! Project version string for PinView.
+FOUNDATION_EXPORT const unsigned char PinViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PinView/PublicHeader.h>
+
+
